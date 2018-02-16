dd2abd55ab032bba46fc70509e0a4e28bffbff41
saki <sakihet@gmail.com>
2018-02-16 09:12:38 +0900

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const program = require('commander')
+const puppeteer = require('puppeteer')
+
+let url = process.argv[2]
+
+async function screenshot(url) {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url)
+  await page.screenshot({ path: `screenshot.png` })
+  await browser.close()
+}
+
+program
+  .version('0.0.1')
+  .command('screenshot <url>')
+  .action((url, cmd) => {
+    screenshot(url)
+  })
+
+program.parse(process.argv)
