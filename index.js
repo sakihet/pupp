35a71d732820c218c2e659db3a60dab8266696e0
saki <sakihet@gmail.com>
2018-02-17 23:54:47 +0900

    complement protocol string

--- index.js
+++ index.js
@@ -2,22 +2,29 @@
 'use strict'
 const program = require('commander')
 const puppeteer = require('puppeteer')
+const { URL } = require('url')
 
-let url = process.argv[2]
+let urlString = process.argv[2]
 
-async function screenshot(url) {
+async function screenshot(urlString) {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.goto(url)
-  await page.screenshot({ path: `screenshot.png` })
+  if (!urlString.match(/^https?\:\/\//)) {
+    urlString = `https://${urlString}`
+  }
+  const url = new URL(urlString)
+  const fileName = `${url.host}.png`
+  await page.goto(urlString)
+  await page.screenshot({ path: fileName })
+  await console.log(`${fileName} saved`)
   await browser.close()
 }
 
 program
   .version('0.0.1')
   .command('screenshot <url>')
-  .action((url, cmd) => {
-    screenshot(url)
+  .action((urlString, cmd) => {
+    screenshot(urlString)
   })
 
 program.parse(process.argv)
